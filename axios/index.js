34210925e4e9682b27c3081fc421eea2b90dcd01
tp <61617660+ittp@users.noreply.github.com>
2022-12-01 00:54:33 +0300

    Update index.js

--- axios/index.js
+++ axios/index.js
@@ -2,9 +2,30 @@
 import useSWR from "swr"
 import axios from 'axios'
 
+import Link from 'next/link'
+
 const fetcher = url => axios.get(url).then(res => res.data)
 
 function App () {
   const { data, error } = useSWR('/api/data', fetcher)
   // ...
 }
+
+
+
+
+function Posts({ posts }) {
+  return (
+    <ul>
+      {posts.map((post) => (
+        <li key={post.id}>
+          <Link href={`/blog/${encodeURIComponent(post.slug)}`}>
+            {post.title}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+export default Posts
