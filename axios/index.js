2da20af492265913d7676af6e9b9b885e30c7461
tp <61617660+ittp@users.noreply.github.com>
2022-11-30 13:08:41 +0300

    Update index.js

--- axios/index.js
+++ axios/index.js
@@ -1,3 +1,10 @@
-const axios = require('axios')
+//const axios = require('axios')
+import useSWR from "swr"
+import axios from 'axios'
 
+const fetcher = url => axios.get(url).then(res => res.data)
 
+function App () {
+  const { data, error } = useSWR('/api/data', fetcher)
+  // ...
+}
